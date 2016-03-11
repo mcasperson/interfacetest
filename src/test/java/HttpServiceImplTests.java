74f1390abf4d9b91004388804300b477f69c41ed
Matthew Casperson <matthewcasperson@gmail.com>
2016-03-12 08:10:56 +1000

    Added a scale test

--- src/test/java/HttpServiceImplTests.java
+++ src/test/java/HttpServiceImplTests.java
@@ -8,6 +8,7 @@ import org.junit.Test;
  */
 public class HttpServiceImplTests {
     private static final HttpService HTTP_SERVICE = new HttpServiceImpl();
+    private static final String SOURCE_URL = "https://localhost/xmlEndpoint";
 
     /**
      * We expect an exception to be thrown for a HTTP address that is not contactable
@@ -16,5 +17,15 @@ public class HttpServiceImplTests {
     @Test(expected = HttpException.class)
     public void testBadUrl() {
         HTTP_SERVICE.callHttpEndpoint("http://256.256.256.256");
+    }
+
+    /**
+     * How well does this implementation scale?
+     */
+    @Test
+    public void testScale() {
+        for (int i = 0; i < 100000; ++i) {
+            HTTP_SERVICE.callHttpEndpoint(SOURCE_URL);
+        }
     }
 }
